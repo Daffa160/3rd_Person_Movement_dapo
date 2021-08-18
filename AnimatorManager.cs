15812b614e2890ad32dd81d77067782b6d8b301d
Daffa Alif Murtaja <87127448+Daffa160@users.noreply.github.com>
2021-08-18 21:05:21 +0700

    Add files via upload

---
+++ AnimatorManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    //parameter id di animator
+    int vertical;
+    int horizontal;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        vertical = Animator.StringToHash("vertical");
+        horizontal = Animator.StringToHash("horizontal");
+    }
+
+    public void UpadateAnimatorValue(float horizontalMovement, float verticalMovemen)//, bool lagiLari)
+    {
+        //animation snapping
+
+        float snappHorizontal;
+        float snappVertical;
+
+        #region Snapped Horizontal
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            snappHorizontal = 0.5f;
+        }
+        else if (horizontalMovement > 0.55f)
+        {
+            snappHorizontal = 1f;
+        }
+        else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            snappHorizontal = -0.5f;
+        }
+        else if (horizontalMovement < -0.55f)
+        {
+            snappHorizontal = -1f;
+        }
+        else
+        {
+            snappHorizontal = 0;
+        }
+        #endregion
+        #region Snapped Vertical
+        if (verticalMovemen > 0 && verticalMovemen < 0.55f)
+        {
+            snappVertical = 0.5f;
+        }
+        else if (verticalMovemen > 0.55f)
+        {
+            snappVertical = 1f;
+        }
+        else if (verticalMovemen < 0 && verticalMovemen > -0.55f)
+        {
+            snappVertical = -0.5f;
+        }
+        else if (verticalMovemen < -0.55f)
+        {
+            snappVertical = -1f;
+        }
+        else
+        {
+            snappVertical = 0;
+        }
+        #endregion
+
+        /*if (lagiLari)
+        {
+            snappHorizontal = horizontalMovement;
+            snappVertical = 2;
+        }*/
+
+        animator.SetFloat(horizontal, snappHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat(vertical, snappVertical, 0.1f, Time.deltaTime);
+    }
+}
