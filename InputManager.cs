b53e8dc4ed1f6cd3b20813dea973f9ce3ed5c78a
Daffa Alif Murtaja <87127448+Daffa160@users.noreply.github.com>
2021-09-02 19:48:38 +0700

    ngerapiin scrip

--- InputManager.cs
+++ InputManager.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerController playerController;
-    AnimatorManager animatorManager;using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InputManager : MonoBehaviour
-{
-    PlayerController playerController;
     AnimatorManager animatorManager;
     PlayerLocomotion playerLocomotion;
     CharacterController controller;
@@ -84,94 +77,6 @@ public class InputManager : MonoBehaviour
         }
         //Debug.Log(moveAmount);
         //Debug.Log(controller.isGrounded);
-    }
-
-    private void HandleSprintingInput()
-    {
-        if (ShiftPress && moveAmount == 1)
-        {
-            playerLocomotion.lari = true;
-            //tambahkan UpdatValueAnimatio pengkondisan bool
-        }
-        else
-        {
-            playerLocomotion.lari = false;
-        }
-    }
-
-    private void HandleJumpInput()
-    {
-        if (SpacePress)
-        {
-            playerLocomotion.loncat = true;
-        }
-        else
-        {
-            playerLocomotion.loncat = false;
-        }
-    }
-
-    
-}
-
-    PlayerLocomotion playerLocomotion;
-
-    public Vector2 input;
-    public float horizontal;
-    public float vertical;
-    public float moveAmount;
-
-    [Header("Indicator")]
-    public bool ShiftPress;
-    public bool SpacePress;
-
-    private void Awake()
-    {
-        animatorManager = GetComponent<AnimatorManager>();
-        playerLocomotion = GetComponent<PlayerLocomotion>();
-    }
-
-    private void OnEnable()
-    {
-        if(playerController == null)
-        {
-            playerController = new PlayerController();
-
-            playerController.PlayerMovement.Movement.performed += i => input = i.ReadValue<Vector2>();
-            //shift
-            playerController.PlayerMovement.Runing.performed += i => ShiftPress = true;
-            playerController.PlayerMovement.Runing.canceled += i => ShiftPress = false;
-
-            //jump
-            playerController.PlayerMovement.Jump.performed += i => SpacePress = true;
-            playerController.PlayerMovement.Jump.canceled += i => SpacePress = false;
-        }
-
-        playerController.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerController.Disable();
-    }
-
-    public void HandleAllInput()
-    {
-        MovementInput();
-        HandleSprintingInput();
-        HandleJumpInput();
-    }
-
-    private void MovementInput()
-    {
-        horizontal = input.x;
-        vertical = input.y;
-
-        //start animasi
-        //rumus menghitung, buat animasi
-        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
-        animatorManager.UpadateAnimatorValue(0f, moveAmount, playerLocomotion.lari);
-        //Debug.Log(moveAmount);
     }
 
     private void HandleSprintingInput()
